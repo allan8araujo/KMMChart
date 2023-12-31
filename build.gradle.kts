0e57d12d9f09dd9ec9a8136b9277b59a786e83c6
allan <allan8araujo@gmail.com>
2023-12-31 13:31:59 -0300

    add detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     id("com.android.library").apply(false)
     id("org.jetbrains.compose").apply(false)
     id("org.jetbrains.kotlin.multiplatform").apply(false) version "1.8.20"
+    id("io.gitlab.arturbosch.detekt") version "1.23.4"
 }
